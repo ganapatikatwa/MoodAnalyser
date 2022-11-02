56bdade75f80d77a5a2f551d7bbe5daef6e63e02
[ganapat] <ganapatisakatwa@gmail.com>
2022-11-02 18:28:40 +0530

    Repeating 1.1 with Constructor

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -3,7 +3,7 @@ namespace MoodAnalyser
     [TestClass]
     public class UnitTest1
     {
-        //Test Case 1.1  Given I am in Sad Mood Message Should return Sad Mood
+        //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
 
         [TestMethod]
         public void GivenSADMood_ShouldReturnSAD()
@@ -25,27 +25,5 @@ namespace MoodAnalyser
             Assert.AreEqual(expected, actual);
         }
 
-
-        //Test Case 1.2  Given I am in Happy Mood Message Should return Happy Mood
-
-        [TestMethod]
-        public void GivenHappyMoodMessage_ShouldReturnHAPPY()
-        {
-
-            //Arrange
-
-            string message = "I am in Happy Mood";
-            MoodAnalyser obj = new MoodAnalyser(message);
-
-            string expected = "Happy Mood";
-
-            //Act
-
-            string actual = obj.AnalyseMood();
-
-            //Assert
-
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
